a8eac570e350e32af042c13fd5e98995a7c38616
gilvan <g.r.almeida@live.com>
2020-10-30 11:17:50 -0300

    Escrevendo o código de suporte http

--- ByteBank.Portal/ByteBank.Portal/Program.cs
+++ ByteBank.Portal/ByteBank.Portal/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace ByteBank.Portal
+﻿using ByteBank.Portal.Infraestrutura;
+using System;
+
+namespace ByteBank.Portal
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var prefixos = new string[]
+            {
+                "http://localhost:5341/"
+            };
+
+            var webApplication = new WebApplication(prefixos);
+            webApplication.Iniciar();
+
+            Console.ReadKey();
         }
     }
 }
